b1b70707624a780222992c5b70711c1951bb3c2c
黄玉慧 <2985380611@qq.com>
2019-07-28 20:20:05 +0800

    详情页轮播图

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img src="https://img1.qunarzz.com/vs_ceph_vs_tts/2832024d-dfe1-4999-b426-497ef7971e3d.jpg_r_640x420x90_56057c8e.jpg" alt="" class="banner-img">
+            <div class="banner-info">
+                <div class="banner-title">自然进去</div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe634;</span>
+                    39
+                </div>
+            </div>
+        </div>
+        <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </div>
+</template>
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: [
+        'https://img1.qunarzz.com/vs_ceph_vs_tts/2832024d-dfe1-4999-b426-497ef7971e3d.jpg_r_640x420x90_56057c8e.jpg',
+        'https://img1.qunarzz.com/vs_ceph_vs_tts/2832024d-dfe1-4999-b426-497ef7971e3d.jpg_r_640x420x90_56057c8e.jpg'
+      ]
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+        width: 100%
+    .banner-info
+        display: flex
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        line-height: .6rem
+        color: #fff
+        background-image: linear-gradient(top, rgba(0,0,0,0),rgba(0,0,0,.8))
+        .banner-title
+            flex: 1
+        .banner-number
+            padding: 0 .4rem
+            height: .32rem
+            line-height: .32rem
+            border-radius: .2rem
+            background: rgba(0,0,0, .8)
+            font-size: .24rem
+            margin-top: .14rem
+            .banner-icon
+                font-size: .24rem
+</style>
--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,7 +2,13 @@
     <div>
         <div class='title'>热销推荐</div>
         <ul>
-            <li class="item border-bottom" v-for="item of list" :key="item.id">
+            <router-link
+            tag="li"
+            class="item border-bottom"
+            v-for="item of list"
+            :key="item.id"
+            :to="'/detail/'+ item.id"
+            >
                 <img  :src="item.imgUrl" alt="" class="item-img"/>
                 <div class="item-info">
                     <p class="item-title">
@@ -10,7 +16,7 @@
                     <p class="item-desc">{{item.desc}}</p>
                     <button class="item-button">查看详情 </button>
                 </div>
-            </li>
+            </router-link>
         </ul>
     </div>
 </template>
